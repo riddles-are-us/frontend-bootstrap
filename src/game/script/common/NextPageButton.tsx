38b8ddd218a66bbfd6fd81f98fea3883f2c5c2ea
charlie275691502 <charlie275691502@gmail.com>
2025-07-08 12:50:18 +1000

    Add page selector template

---
+++ src/game/script/common/NextPageButton.tsx
@@ -0,0 +1,43 @@
+import ImageButton from "./ImageButton";
+
+interface Props {
+  onClick: () => void;
+  isDisabled: boolean;
+  normalImage: string;
+  hoverImage: string;
+  clickImage: string;
+}
+
+const NextPageButton = ({
+  onClick,
+  normalImage,
+  hoverImage,
+  clickImage,
+  isDisabled,
+}: Props) => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        width: "auto",
+        height: "100%",
+        aspectRatio: "29 / 39",
+        transform: "translate(-50%, -50%)",
+        margin: "0px",
+      }}
+    >
+      <ImageButton
+        onClick={onClick}
+        isDisabled={isDisabled}
+        defaultImagePath={normalImage}
+        hoverImagePath={hoverImage}
+        clickedImagePath={clickImage}
+        disabledImagePath={clickImage}
+      />
+    </div>
+  );
+};
+
+export default NextPageButton;
---
+++ src/game/script/common/PageSelector.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./PageSelector.css";
+import NextPageButton from "./NextPageButton";
+import PrevPageButton from "./PrevPageButton";
+
+interface Props {
+  currentPage: number;
+  totalPage: number;
+  nextPageNormalImage: string;
+  nextPageHoverImage: string;
+  nextPageClickImage: string;
+  prevPageNormalImage: string;
+  prevPageHoverImage: string;
+  prevPageClickImage: string;
+  pageSelectorFrame: string;
+  onClickPrevPageButton: () => void;
+  onClickNextPageButton: () => void;
+}
+
+const PageSelector = ({
+  currentPage,
+  totalPage,
+  nextPageNormalImage,
+  nextPageHoverImage,
+  nextPageClickImage,
+  prevPageNormalImage,
+  prevPageHoverImage,
+  prevPageClickImage,
+  pageSelectorFrame,
+  onClickPrevPageButton,
+  onClickNextPageButton,
+}: Props) => {
+  const enableNextPageButton = currentPage < totalPage - 1;
+  const enablePrevPageButton = currentPage > 0;
+  const containerRef = useRef<HTMLParagraphElement>(null);
+  const [fontSize, setFontSize] = useState<number>(0);
+
+  const adjustSize = () => {
+    if (containerRef.current) {
+      setFontSize(containerRef.current.offsetHeight / 3.5);
+    }
+  };
+
+  useEffect(() => {
+    adjustSize();
+
+    window.addEventListener("resize", adjustSize);
+    return () => {
+      window.removeEventListener("resize", adjustSize);
+    };
+  }, [containerRef.current]);
+
+  return (
+    <div className="page-selector-container" ref={containerRef}>
+      <div className="page-selector-prev-button">
+        <PrevPageButton
+          isDisabled={!enablePrevPageButton}
+          normalImage={prevPageNormalImage}
+          hoverImage={prevPageHoverImage}
+          clickImage={prevPageClickImage}
+          onClick={onClickPrevPageButton}
+        />
+      </div>
+      <img
+        src={pageSelectorFrame}
+        className="page-selector-page-number-background"
+      ></img>
+
+      <p
+        className="page-selector-page-number-text"
+        style={{ fontSize: fontSize }}
+      >{`${currentPage + 1} / ${totalPage}`}</p>
+      <div className="page-selector-next-button">
+        <NextPageButton
+          isDisabled={!enableNextPageButton}
+          normalImage={nextPageNormalImage}
+          hoverImage={nextPageHoverImage}
+          clickImage={nextPageClickImage}
+          onClick={onClickNextPageButton}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PageSelector;
